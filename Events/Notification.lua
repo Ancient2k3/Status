2c77e56fa33f942d59b0c7ee22d76b42f1649fe3
DE <trinhro2022@gmail.com>
2026-08-07 22:49:31 +0700

    Refactor notification logic and UI elements
    Refactored notification handling by creating a separate function 're_active_ntf' to manage event connections. Updated UI elements for better organization and readability.

--- Events/Notification.lua
+++ Events/Notification.lua
@@ -2,9 +2,11 @@ function gsv(name) return game:GetService(name) end
 function ins(...) return Instance.new(...) end
 local core = gsv"CoreGui"
 local rs = gsv"RunService"
-local ui_t, _G.ntf_content = {}, ""
+local ui_t = {}
+_G.ntf_content = ""
 ui_t.screen = ins("ScreenGui", core)
 ui_t.btn = ins("TextButton", ui_t.screen)
+ui_t.corner = ins("UICorner", ui_t.btn)
 ui_t.pre = "Thông báo:\n"
 ui_t.is_fading = false
 ui_t.event = nil
@@ -12,6 +14,7 @@ ui_t.current = 1
 ui_t.adjust = 100
 
 local b = ui_t.btn
+ui_t.screen.Name = "NEW_UPDATES_NOTIFY"
 b.Name = "New:Events"
 b.BackgroundTransparency = 0.5
 b.BackgroundColor3 = Color3.new(0, 0, 0)
@@ -25,6 +28,19 @@ b.Font = Enum.Font.Code
 b.TextXAlignment = "Left"
 b.TextYAlignment = "Top"
 b.Visible = true
+ui_t.corner.CornerRadius = UDim.new(0.05, 0)
+
+function re_active_ntf()
+  ui_t.event = rs.RenderStepped:Connect(function()
+    if _G.ntf_content ~= "" and b.Text == ui_t.pre then
+      b.Text = ui_t.pre .. _G.ntf_content .. "\n(Chạm vào để tắt thông báo)"
+      if b.Text ~= ui_t.pre then
+        ui_t.event:Disconnect()
+        ui_t.event = nil
+      end
+    end
+  end)
+end
 
 b.MouseLeave:Connect(function()
   if not ui_t.is_fading then ui_t.is_fading = true
@@ -32,15 +48,10 @@ b.MouseLeave:Connect(function()
       b.BackgroundTransparency = ui_t.current / ui_t.adjust
     until b.BackgroundTransparency >= 1
     b.Visible = false
+    b.Text = ui_t.pre
+    _G.ntf_content = ""
+    re_active_ntf()
   end
 end)
 
-ui_t.event = rs.RenderStepped:Connect(function()
-  if _G.ntf_content ~= "" and b.Text == ui_t.pre then
-    b.Text = ui_t.pre .. _G.ntf_content
-    if b.Text ~= ui_t.pre then
-      ui_t.event:Disconnect()
-      ui_t.event = nil
-    end
-  end
-end)
+re_active_ntf()

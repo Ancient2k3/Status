ef0688ed180fccd2ec1e8d59f56ea404d94237e7
DE <trinhro2022@gmail.com>
2026-08-08 10:12:29 +0700

    Implement time formatting function
    Added a function to format time in days, hours, minutes, and seconds.

--- Events/Notification.lua
+++ Events/Notification.lua
@@ -9,6 +9,7 @@ ui_t.btn = ins("TextLabel", ui_t.screen)
 ui_t.corner = ins("UICorner", ui_t.btn)
 ui_t.stroke = ins("UIStroke", ui_t.btn)
 ui_t.pre = " Thông báo:\n"
+ui_t.old_pre = ""
 ui_t.is_fading = false
 ui_t.event = nil
 ui_t.current = 1
@@ -32,10 +33,18 @@ b.Visible = false
 ui_t.corner.CornerRadius = UDim.new(0.05, 0)
 ui_t.stroke.Thickness = 2
 
+function format_time(s)
+  local d = math.floor(s / 86400) s = s % 86400
+  local h = math.floor(s / 3600) s = s % 3600
+  local m = math.floor(s / 60) s = s % 60
+  return string.format("%d:%02d:%02d:%02d", d, h, m, s)
+end
+
 function re_active_ntf()
   ui_t.event = rs.RenderStepped:Connect(function()
     if _G.ntf_content ~= "" and b.Text == ui_t.pre then
       b.Text = ui_t.pre .. _G.ntf_content .. "\n (Chạm vào để tắt thông báo)"
+      ui_t.old_pre = b.Text
       if b.Text ~= ui_t.pre then
         b.BackgroundTransparency = 0.5
         b.Visible = true

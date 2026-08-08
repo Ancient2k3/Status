9087dd74b3b3f469bdfbff12ec80f56003b03f5e
DE <trinhro2022@gmail.com>
2026-08-08 10:20:28 +0700

    Enhance notification with dynamic time display
    Added a new event handler to update notification time dynamically.

--- Events/Notification.lua
+++ Events/Notification.lua
@@ -12,6 +12,7 @@ ui_t.pre = " Thông báo:\n"
 ui_t.old_pre = ""
 ui_t.is_fading = false
 ui_t.event = nil
+ui_t.event_1 = nil
 ui_t.current = 1
 ui_t.adjust = 100
 
@@ -34,20 +35,28 @@ ui_t.corner.CornerRadius = UDim.new(0.05, 0)
 ui_t.stroke.Thickness = 2
 
 function format_time(s)
+  if s < 0 then return "nil" end
   local d = math.floor(s / 86400) s = s % 86400
   local h = math.floor(s / 3600) s = s % 3600
   local m = math.floor(s / 60) s = s % 60
   return string.format("%d:%02d:%02d:%02d", d, h, m, s)
 end
 
+function add_ninja()
+  ui_t.event_1 = rs.RenderStepped:Connect(function()
+    b.Text = ui_t.old_pre .. format_time(_G.ntf_time - tick()) .. ")"
+  end)
+end
+
 function re_active_ntf()
   ui_t.event = rs.RenderStepped:Connect(function()
     if _G.ntf_content ~= "" and b.Text == ui_t.pre then
-      b.Text = ui_t.pre .. _G.ntf_content .. "\n (Chạm vào để tắt thông báo)"
+      b.Text = ui_t.pre .. _G.ntf_content .. "\n (Thời gian còn: "
       ui_t.old_pre = b.Text
       if b.Text ~= ui_t.pre then
         b.BackgroundTransparency = 0.5
         b.Visible = true
+        add_ninja()
         ui_t.event:Disconnect()
         ui_t.event = nil
       end
@@ -60,6 +69,8 @@ b.MouseLeave:Connect(function()
     repeat ui_t.current = ui_t.current + 1 task.wait(0.01)
       b.BackgroundTransparency = ui_t.current / ui_t.adjust
     until b.BackgroundTransparency >= 1
+    ui_t.event_1:Disconnect()
+    ui_t.old_pre = ""
     b.Visible = false
     b.Text = ui_t.pre
     ui_t.current = 1
